16378be012a5ab9d403c02c3db5f6ce16c65e8cf
Nguyen177013 <phannguyendoremon@gmail.com>
2023-05-25 17:53:58 +0700

    first commit

---
+++ server/src/main/java/com/ecchi/storages/nukecode/NukecodeApplication.java
@@ -0,0 +1,13 @@
+package com.ecchi.storages.nukecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NukecodeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ecchi/storages/nukecode/NukecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecchi.storages.nukecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
