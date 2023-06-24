606f56e34b19a3bb8592284347cbd0d12ef0509a
Nguyen177013 <phannguyendoremon@gmail.com>
2023-06-25 00:42:16 +0700

    idk what the fuck I am doing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,41 @@
-import { Image, Layout, Menu } from "antd"
-import { SmileOutlined } from "@ant-design/icons"
+import { Image, Layout, Menu, MenuProps } from "antd"
+import { HomeOutlined, FileImageOutlined, CodeOutlined } from "@ant-design/icons"
 import { useState } from "react";
 const { Header, Sider, Content } = Layout;
-import "./assets/css/index.css"
+import "../public/css/index.css"
 function App() {
   const [collapsed, setCollapsed] = useState<boolean>(false);
   const toggle = () => {
     setCollapsed(preCollapsed => !preCollapsed);
   }
+  const menuItems: MenuProps['items'] = [
+    {
+      label:"Home",
+      icon: <HomeOutlined/>,
+      key:"home"
+    },
+    {
+      label:"Images",
+      icon: <FileImageOutlined/>,
+      key:"images"
+    },
+    {
+      label:"Nuke Code",
+      icon: <CodeOutlined/>,
+      key:"code"
+    }
+  ];
   return (
     <>
       <Layout>
-        <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <Header style={{ display: 'flex', alignItems: 'center', backgroundColor:"#fff", padding:"0" }}>
           <div className="demo-logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"            
-          />
-          <Image
-          width={200}
-          src="./assets/img/image-8.png"
-          preview = {false}
-          />
+            <Image
+              width={200}
+              src="/img/image-8.png"
+              preview={false}
+            />
+          <h1 style={{}}>My Private Storage</h1>
         </Header>
       </Layout>
       <Layout>
@@ -29,37 +43,31 @@ function App() {
           trigger={null}
           collapsed={collapsed}
           collapsible
+          style={{
+            padding:"10px 0",
+            overflow: "auto",
+            height: "calc(100vh - 64px)",
+            position: "sticky",
+            top: 0,
+            left: 0
+          }}
         >
           <div className="logo" />
           <Menu
             theme="dark" mode="inline" defaultSelectedKeys={['1']}
+            items={menuItems}
           >
-            <Menu.ItemGroup>
-
-            </Menu.ItemGroup>
-            <Menu.Item key={1}>
-              <span>Home</span>
-            </Menu.Item>
-            <Menu.Item key={2}>
-              <SmileOutlined type="video-camera" />
-              <span>Image</span>
-            </Menu.Item>
-            <Menu.Item key={3}>
-              <SmileOutlined type="upload" />
-              <span>Source</span>
-            </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <SmileOutlined
+          {/* <Header style={{ background: '#fff', padding: 0 }}>
+            <HomeOutlined
               className="trigger"
               type={collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={toggle}
             />
-          </Header>
+          </Header> */}
           <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
-            Content
           </Content>
         </Layout>
       </Layout>
