97537f81123d0d3b908e675a135825b797fe51a8
Nguyen177013 <phannguyendoremon@gmail.com>
2023-06-30 14:08:54 +0700

    cors is suck

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,27 @@
 import { Col, Row } from "antd";
+import axios from "axios";
+import { useEffect } from "react";
+import CountUp from 'react-countup';
 
 const HomePage = () => {
+    useEffect(()=>{
+        // axios.get("http://localhost:8080/hentaibu/api/sauce-history/get-history?year=2023&dateUpload=month",{
+        //     headers:{
+        //         "Content-Type":"application/json",
+        //         Authorization:"Basic aGVudGFpYnU6NTA3YzZlMzRiNzdiNTkxNmMzYjc5MWUyZmY2MjcxMTQ="
+        //     }
+        // })
+        // .then(response =>{
+        //     console.log(response);
+        // })
+        fetch("http://localhost:8080/hentaibu/api/sauce-history/get-history?year=2023&dateUpload=month",{
+            method:"GET",
+            headers:{
+                "content-type": "application/json",
+                "Authorization":"Basic aGVudGFpYnU6NTA3YzZlMzRiNzdiNTkxNmMzYjc5MWUyZmY2MjcxMTQ="
+            }
+    })
+    },[])
     return (
         <div>
             <h1>
@@ -10,7 +31,7 @@ const HomePage = () => {
                 <Col span={6}>
                     <div>
                         <div>
-                            <h4> </h4>
+                            <h4> <CountUp end={100} duration={3}></CountUp> </h4>
                         </div>
                     </div>
                 </Col>
--- server/src/main/java/com/example/storage/ecchi/controller/SauceHistoryController.java
+++ server/src/main/java/com/example/storage/ecchi/controller/SauceHistoryController.java
@@ -3,6 +3,7 @@ package com.example.storage.ecchi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import com.example.storage.ecchi.model.TotalSauceHistory;
 import com.example.storage.ecchi.service.SauceHistoryService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/api/sauce-history")
 public class SauceHistoryController {
 
