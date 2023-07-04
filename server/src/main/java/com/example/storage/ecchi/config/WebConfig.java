f84c5dedfc0f017d05aa544a18a378cc8931ace0
Nguyen177013 <phannguyendoremon@gmail.com>
2023-07-04 11:12:55 +0700

    completed cors

--- server/src/main/java/com/example/storage/ecchi/config/WebConfig.java
+++ server/src/main/java/com/example/storage/ecchi/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.example.storage.ecchi.config;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -18,18 +20,31 @@ public class WebConfig {
 
 	@Bean
 	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://127.0.0.1:5173/");
-		config.setAllowedHeaders(
-				Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
-		config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-				HttpMethod.DELETE.name()));
-		config.setMaxAge(3600L);
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-		bean.setOrder(-102);
-		return bean;
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOriginPattern("*");
+	    config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
+	    config.setAllowedMethods(Arrays.asList(
+	        HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()));
+	    
+	    // Calculate the dynamic Max-Age value
+	    long maxAgeSeconds = calculateMaxAge();
+
+	    // Set the dynamic Max-Age value
+	    config.setMaxAge(maxAgeSeconds);
+
+	    source.registerCorsConfiguration("/**", config);
+	    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(-102);
+	    return bean;
+	}
+
+	// Helper method to calculate the Max-Age value based on the current time
+	private long calculateMaxAge() {
+	    LocalDateTime currentTime = LocalDateTime.now();
+	    LocalDateTime expirationTime = currentTime.plusHours(1); // Set the expiration time to 1 hour from the current time
+	    Duration duration = Duration.between(currentTime, expirationTime);
+	    return duration.getSeconds();
 	}
 }
